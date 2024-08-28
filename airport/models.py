ff046b222f1ed00aadf768b4d7e370b82f9d52ef
Maksym Nimych <maksimnimitch@gmail.com>
2024-08-28 23:18:52 +0300

    Add AirplaneType and Airplane models

--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        AirplaneType,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
