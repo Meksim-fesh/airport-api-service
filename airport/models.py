fa59709b8546064261f337c76f19109a3f651bd5
Maksym Nimych <maksimnimitch@gmail.com>
2024-09-01 19:03:48 +0300

    Add Country, City & Airport models and make migrations

--- airport/models.py
+++ airport/models.py
@@ -18,3 +18,34 @@ class Airplane(models.Model):
         blank=True,
         on_delete=models.SET_NULL,
     )
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    country = models.ForeignKey(
+        Country,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.country})"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.ForeignKey(
+        City,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
+
+    def __str__(self) -> str:
+        return self.name
