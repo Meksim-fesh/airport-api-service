c1c55011040c612bdc3a16810b6722096a9431c5
Maksym Nimych <maksimnimitch@gmail.com>
2024-09-01 16:27:44 +0300

    Create general serializers for AirplaneType & Airplane models

---
+++ airport/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from airport.models import Airplane, AirplaneType
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name")
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = (
+            "id",
+            "name",
+            "rows",
+            "seats_in_row",
+            "airplane_type",
+        )
