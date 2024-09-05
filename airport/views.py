0e626a6c6916c78e53cdf5a5efb8624ff64ccb87
Maksym Nimych <maksimnimitch@gmail.com>
2024-09-05 23:45:01 +0300

    Create basic views for Route, Crew, Flight, Ticket & Order models

--- airport/views.py
+++ airport/views.py
@@ -1,29 +1,53 @@
 from rest_framework.viewsets import ModelViewSet
 
-from airport.models import Airplane, AirplaneType, Airport, City, Country
-from airport.serializers import AirplaneSerializer, AirplaneTypeSerializer, AirportSerializer, CitySerializer, CountrySerializer
+from airport import models, serializers
 
 
 class AirplaneTypeViewSet(ModelViewSet):
-    queryset = AirplaneType.objects.all()
-    serializer_class = AirplaneTypeSerializer
+    queryset = models.AirplaneType.objects.all()
+    serializer_class = serializers.AirplaneTypeSerializer
 
 
 class AirplaneViewSet(ModelViewSet):
-    queryset = Airplane.objects.all()
-    serializer_class = AirplaneSerializer
+    queryset = models.Airplane.objects.all()
+    serializer_class = serializers.AirplaneSerializer
 
 
 class CountryViewSet(ModelViewSet):
-    queryset = Country.objects.all()
-    serializer_class = CountrySerializer
+    queryset = models.Country.objects.all()
+    serializer_class = serializers.CountrySerializer
 
 
 class CityViewSet(ModelViewSet):
-    queryset = City.objects.all()
-    serializer_class = CitySerializer
+    queryset = models.City.objects.all()
+    serializer_class = serializers.CitySerializer
 
 
 class AirportViewSet(ModelViewSet):
-    queryset = Airport.objects.all()
-    serializer_class = AirportSerializer
+    queryset = models.Airport.objects.all()
+    serializer_class = serializers.AirportSerializer
+
+
+class RouteViewSet(ModelViewSet):
+    queryset = models.Route.objects.all()
+    serializer_class = serializers.RouteSerializer
+
+
+class FlightViewSet(ModelViewSet):
+    queryset = models.Flight.objects.all()
+    serializer_class = serializers.FlightSerializer
+
+
+class CrewViewSet(ModelViewSet):
+    queryset = models.Crew.objects.all()
+    serializer_class = serializers.CrewSerializer
+
+
+class TicketViewSet(ModelViewSet):
+    queryset = models.Ticket.objects.all()
+    serializer_class = serializers.TicketSerializer
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
