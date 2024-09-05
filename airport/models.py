67441e57b0f995f841fc58ff92d481f243995d14
Maksym Nimych <maksimnimitch@gmail.com>
2024-09-05 21:03:02 +0300

    Create Route, Flight & Crew models and make migrations

--- airport/models.py
+++ airport/models.py
@@ -19,6 +19,9 @@ class Airplane(models.Model):
         on_delete=models.SET_NULL,
     )
 
+    def __str__(self) -> str:
+        return f"{self.name} ({self.airplane_type})"
+
 
 class Country(models.Model):
     name = models.CharField(max_length=255, unique=True)
@@ -49,3 +52,51 @@ class Airport(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="source_routes",
+    )
+    destination = models.ForeignKey(
+        Airport,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="destination_routes",
+    )
+    distance = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.source} - {self.destination}"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Flight(models.Model):
+    crew = models.ManyToManyField(Crew)
+    route = models.ForeignKey(
+        Route,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="routes",
+    )
+    airplane = models.ForeignKey(
+        Airplane,
+        null=True,
+        on_delete=models.CASCADE,
+        related_name="airplanes",
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.route} ({self.departure_time})"
