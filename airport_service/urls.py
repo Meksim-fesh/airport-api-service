8b7e834150d78a08159dc481b451ebb0277c14fd
Maksym Nimych <maksimnimitch@gmail.com>
2024-09-07 15:59:24 +0300

    Install and configure django_debug_toolbar

--- airport_service/urls.py
+++ airport_service/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from debug_toolbar.toolbar import debug_toolbar_urls
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/airport/", include("airport.urls", namespace="airport"))
-]
+] + debug_toolbar_urls()
